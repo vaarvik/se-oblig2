817282d73e464475a94cc0a0efdbc6680064e6c6
vaarvik <johannvaarvik@hotmail.com>
2020-10-07 22:20:54 +0200

    Years that can be divided by 400 are now leap years. Tests run using Stream and parameterized test.

--- src/test/java/When_it_is_leap_year.java
+++ src/test/java/When_it_is_leap_year.java
@@ -1,10 +1,31 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.of;
 
 public class When_it_is_leap_year {
-    @Test
-    public void check_years() {
-        final int leapYear = 104;
-        Assertions.assertTrue(Program.isLeapYear(leapYear));
+
+    private static Stream<Arguments> yearStream(){
+        return Stream.of(
+                of(true, 4),
+                of(false, 10),
+                of(false, 100),
+                of(true, 104),
+                of(false, 200),
+                of(false, 300),
+                of(true, 400),
+                of(false, 1000),
+                of(true, 2004)
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("yearStream")
+    public void check_years(boolean expected, int input) {
+        Assertions.assertEquals(expected, Program.isLeapYear(input));
     }
 }
