5931934815454848c16dd8bfb25b347fea007db9
vaarvik <johannvaarvik@hotmail.com>
2020-10-07 21:03:29 +0200

    isLeapYear returns true if a given year can be divided by 4 but not 100.

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,4 +1,8 @@
 public class Program {
     public static void main(String[] args) {
     }
+
+    public static Boolean isLeapYear(Integer year){
+        return year % 4 == 0 && year % 100 != 0;
+    }
 }
