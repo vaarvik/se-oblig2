d16cfda4a875d27aa6a108821d42e43f1cc3ab56
vaarvik <johannvaarvik@hotmail.com>
2020-10-23 21:14:36 +0200

    Added tests for each leap year criteria.

--- src/test/java/When_it_is_leap_year.java
+++ src/test/java/When_it_is_leap_year.java
@@ -9,32 +9,76 @@ import static org.junit.jupiter.params.provider.Arguments.of;
 
 public class When_it_is_leap_year {
 
-    private static Stream<Arguments> yearStream(){
+    private static Stream<Arguments> fourButNotHundreds(){
         return Stream.of(
                 of(true, 4),
-                of(false, 10),
+                of(true, 16),
                 of(false, 100),
-                of(true, 104),
-                of(false, 200),
-                of(false, 300),
+                of(false, 300)
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("fourButNotHundreds")
+    public void leap_years_are_divisible_by_four_but_not_by_100 (boolean expected, int input) {
+        Assertions.assertEquals(expected, Program.isLeapYear(input));
+    }
+
+    private static Stream<Arguments> fourHundreds(){
+        return Stream.of(
+            of(true, 400),
+            of(true, 1200),
+            of(true, 2000)
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("fourHundreds")
+    public void leap_years_are_divisible_by_four_hundred(boolean expected, int input) {
+        Assertions.assertEquals(expected, Program.isLeapYear(input));
+    }
+
+    private static Stream<Arguments> notFour(){
+        return Stream.of(
+                of(false, 10),
+                of(false, 19),
+                of(false, 101)
+        );
+    }
+    @ParameterizedTest()
+    @MethodSource("notFour")
+    public void leap_years_are_not_divisible_by_four(boolean expected, int input) {
+        Assertions.assertEquals(expected, Program.isLeapYear(input));
+    }
+
+    private static Stream<Arguments> fourHundredsButNotHundreds(){
+        return Stream.of(
                 of(true, 400),
                 of(false, 1000),
                 of(false, 1700),
                 of(false, 1800),
                 of(false, 1900),
                 of(true, 2000),
-                of(true, 2004),
-                of(false, 2100),
-                of(false, 4000),
-                of(true, 8004),
-                of(true, 7048),
-                of(false, 12000)
+                of(false, 2100)
         );
     }
 
     @ParameterizedTest()
-    @MethodSource("yearStream")
-    public void check_years(boolean expected, int input) {
+    @MethodSource("fourHundredsButNotHundreds")
+    public void leap_years_are_divisible_by_four_hundreds_but_not_hundreds(boolean expected, int input) {
+        Assertions.assertEquals(expected, Program.isLeapYear(input));
+    }
+
+    private static Stream<Arguments> fourThousands(){
+        return Stream.of(
+            of(false, 4000),
+            of(false, 12000)
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("fourThousands")
+    public void leap_years_are_not_divisible_by_four_thousands(boolean expected, int input) {
         Assertions.assertEquals(expected, Program.isLeapYear(input));
     }
 }
